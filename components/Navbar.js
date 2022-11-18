ab2c3438e92e584a26501c156cf1b5ec948c8dc8
Revant Cherian Idikulay <RevantCI@users.noreply.github.com>
2022-11-19 01:36:18 +0530

    login & logout

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useContext } from "react";
+import AuthContext from "../stores/authContext";
 
 export default function Navbar() {
+  const { user, login, logout } = useContext(AuthContext);
+  console.log(user);
   return (
     <div className="container">
       <nav>
@@ -13,6 +17,12 @@ export default function Navbar() {
           </li>
           <li>
             <Link href="/guides">Guides</Link>
+          </li>
+          <li onClick={login} className="btn">
+            Login/Signup
+          </li>
+          <li onClick={logout} className="btn">
+            Log Out
           </li>
         </ul>
       </nav>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import type { AppProps } from 'next/app'
 import Navbar from '../components/Navbar'
+import { AuthContextProvider } from '../stores/authContext'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <AuthContextProvider>
       <Navbar />
       <Component {...pageProps} />
-    </div>
+    </AuthContextProvider>
   )
 }
 
